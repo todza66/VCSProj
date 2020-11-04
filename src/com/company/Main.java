a3efd14159251254903801fa1c915ff62d216775
todza66 <t.d.maswera@gmail.com>
2020-11-04 23:47:24 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+	    //Another comment
         System.out.println("Computer Science Rocks!");
         System.out.println("Programming for CS!");
 	    System.out.println("Added from GitHub!");
