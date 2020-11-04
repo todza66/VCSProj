79b83c3c872cb649ad286c1e70ff20b6befcbfd6
tonderai <t.d.maswera@gmail.com>
2020-11-04 23:16:51 +0000

    Added line number 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
         System.out.println("Computer Science Rocks!");
+        System.out.println("Programming for CS!");
     }
 }
